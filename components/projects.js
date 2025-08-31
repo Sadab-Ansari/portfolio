0d2bab8083cc90ed7a342f272f40ff8d6d82beeb
Shadab <sadabansari451@gmail.com>
2025-08-31 23:19:32 +0530

    done

--- components/projects.js
+++ components/projects.js
@@ -38,7 +38,7 @@ export default function Projects() {
 
   return (
     <section id="projects" className="px-6 md:px-16 py-12 bg-transparent">
-      <div className="mb-8 ml-28 flex items-center gap-3">
+      <div className="mb-8 ml-14 flex items-center gap-3">
         {/* Gradient Line before heading */}
         <span className="h-[2px] w-12 bg-gradient-to-r from-purple-500 to-pink-500 rounded" />
 
